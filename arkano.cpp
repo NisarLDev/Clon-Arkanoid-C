95ed7e798bab4b7cdf1ee25e14d2f78f92b037af
NisarLDev <HarusBadah@proton.me>
2022-12-07 12:55:32 +0100

    Defined Programme Basical Structure

--- arkano.cpp
+++ arkano.cpp
@@ -13,6 +13,20 @@ using namespace std;
 #define height 740
 
 int delay=100;
+int lives=5;
+int level=1;
+int score=0;
+bool StartedGame=false;
+bool GameOver=false;
+bool NextLevel=false;
+int DirY=-1;
+int DirX=1;
+int Speed=3;
+int InitialSpeed=3;
+int BackgroundNumber=1;
+bool Dead=false;
+int DeadSequence=1;
+
 
 int main()
 {
