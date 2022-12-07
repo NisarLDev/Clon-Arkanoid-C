2cbdb7b468b38c6415a3791b1c00790b16cf037a
NisarLDev <HarusBadah@proton.me>
2022-12-07 11:55:13 +0100

    First file

---
+++ arkano.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <conio.h>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fstream>
+
+using namespace std;
